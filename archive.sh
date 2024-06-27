f83181bdea863bb1d510e6c395817eb08c6a3a02
David Millman <dave@cs.unc.edu>
2024-06-26 19:17:48 -0600

    Remove jq from script (only a dev dep)

--- archive.sh
+++ archive.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash --pure
-#! nix-shell -p bash mustache-go nix cacert docker jq
+#! nix-shell -p bash mustache-go nix cacert docker
 #! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/tarball/nixos-24.05
 
 set -e
