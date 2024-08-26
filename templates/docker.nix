ddd45a273412465dafb20672ae3d02657a2798fa
Matteo Bjornsson <matteobjornsson@gmail.com>
2024-08-26 13:53:12 -0400

    throw error if cmd is less than one

--- templates/docker.nix
+++ templates/docker.nix
@@ -10,9 +10,13 @@ let
     src = src;
   };
 
+  # Ensure that the cmd list is not empty and construct the cmdConfig
+  cmdConfig = if builtins.length cmd < 1 then
+    builtins.throw "Error: The cmd list must contain at least one element."
+  else
   # Prefix the executable command (first element in the cmd list) with the
-  # executable path
-  cmdConfig = [ (aPkg + "/bin/" + builtins.elemAt cmd 0) ]
+  # executable path, then append the rest of the cmd list
+    [ (aPkg + "/bin/" + builtins.elemAt cmd 0) ]
     ++ builtins.slice cmd 1 (builtins.length cmd);
 
 in pkgs.dockerTools.buildImage {
