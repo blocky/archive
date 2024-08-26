7ad4f2fd9b9242daee4eceeb5acae93f33ff779b
Matteo Bjornsson <matteobjornsson@gmail.com>
2024-08-26 13:41:05 -0400

    update docker.nix and nixfmt

--- templates/docker.nix
+++ templates/docker.nix
@@ -1,23 +1,24 @@
 { cmd, src, imageName, tagName }:
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
-  aPkg = import ./go.nix { pkgs = pkgs; src = src; };
-  cmdFromPkg = aPkg + "/bin/" + builtins.head cmd;
+  pkgs = import nixpkgs {
+    config = { };
+    overlays = [ ];
+  };
+  aPkg = import ./go.nix {
+    pkgs = pkgs;
+    src = src;
+  };
 
-  cmdTail = builtins.tail cmd;
-  cmdConfig = if builtins.length cmdTail > 0 then
-    [ "${cmdFromPkg}" ] ++ cmdTail
-  else
-    [ "${cmdFromPkg}" ];
+  # Prefix the executable command (first element in the cmd list) with the
+  # executable path
+  cmdConfig = [ (aPkg + "/bin/" + builtins.elemAt cmd 0) ]
+    ++ builtins.slice cmd 1 (builtins.length cmd);
 
-in
-pkgs.dockerTools.buildImage {
-    name = "${imageName}";
-    tag = "${tagName}";
-    copyToRoot = [ aPkg ];
+in pkgs.dockerTools.buildImage {
+  name = "${imageName}";
+  tag = "${tagName}";
+  copyToRoot = [ aPkg ];
 
-    config = {
-        Cmd = cmdConfig;
-    };
+  config = { Cmd = cmdConfig; };
 }
