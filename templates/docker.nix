eac6b7c02d4a9cc878f6bb2d3cf329f07d935147
Matteo Bjornsson <matteobjornsson@gmail.com>
2024-08-26 16:01:39 -0400

    update nix syntax

--- templates/docker.nix
+++ templates/docker.nix
@@ -11,13 +11,12 @@ let
   };
 
   # Ensure that the cmd list is not empty and construct the cmdConfig
-  cmdConfig = if builtins.length cmd < 1 then
+  cmdConfig = if cmd == [] then
     builtins.throw "Error: The cmd list must contain at least one element."
   else
   # Prefix the executable command (first element in the cmd list) with the
   # executable path, then append the rest of the cmd list
-    [ (aPkg + "/bin/" + builtins.elemAt cmd 0) ]
-    ++ builtins.slice cmd 1 (builtins.length cmd);
+    [ (aPkg + "/bin/" + builtins.elemAt cmd 0) ] ++ builtins.tail cmd;
 
 in pkgs.dockerTools.buildImage {
   name = "${imageName}";
