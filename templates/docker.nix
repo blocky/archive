130438ccf4e8b7ced94c74baa5f31c015e7b77e8
David Millman <dave@cs.unc.edu>
2024-09-12 16:43:19 -0600

    Revert "Add cacerts"
    This reverts commit 2968a0659664ab49121c14e4deceff6adbd1e8eb.

--- templates/docker.nix
+++ templates/docker.nix
@@ -40,6 +40,6 @@ let
 in pkgs.dockerTools.buildImage {
   name = "${imageName}";
   tag = "${tagName}";
-  copyToRoot = [ slimGoProj pkgs.cacert ];
+  copyToRoot = [ slimGoProj ];
   config = { Cmd = dockerCMD; };
 }
